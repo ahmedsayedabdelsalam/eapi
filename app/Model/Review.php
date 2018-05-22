9085f4a8841fe6c4c8bf74a2506118687ed53d4e
ahmedsayedll <e.eng.ahmedsayed@gmail.com>
2018-05-22 15:40:30 +0200

    Make Relationships

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,10 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Product;
 class Review extends Model
 {
-    //
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
