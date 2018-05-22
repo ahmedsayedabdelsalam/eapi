88b5f2f1639866b8b488ec63e133b17ce5ba831b
ahmedsayedll <e.eng.ahmedsayed@gmail.com>
2018-05-22 22:04:19 +0200

    Modify Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'newPrice' => round((1-$this->discount/100) * $this->price),
+            // 'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No Rating Yet',
+            'ratingd' => round($this->reviews->avg('star'), 2) == 0 ? 'No Rating Yet' : round($this->reviews->avg('star'), 2),
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         factory(Product::class, 50)->create();
-        factory(Review::class, 300)->create();
+            factory(Review::class, 300)->create();
     }
 }
