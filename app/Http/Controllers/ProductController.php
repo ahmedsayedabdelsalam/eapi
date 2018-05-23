6a17197b7773c6a714c4710502b0ea0a20dcb82d
Mohamed Abdalmoniem <mabdalmoniem8@gmail.com>
2018-05-23 12:53:18 +0300

    Product Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,16 @@ use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
+use App\Http\Requests\ProductRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +42,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->discount = $request->discount;
+        $product->save();
+        return response([
+            "data" => new ProductResource($product) 
+        ], Response::HTTP_CREATED);
     }
 
     /**
@@ -71,7 +87,12 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail'] = $request->description;
+        unset($product->description);
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ], Response::HTTP_CREATED);
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,7 @@ use App\Model\Review;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'detail','price', 'stock', 'discount'];
     public function reviews() {
         return $this->hasMany(Review::class);
     }
