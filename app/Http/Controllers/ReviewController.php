f034e6baf1ea36bca09545d2ec84170e4d779d7e
Mohamed Abdalmoniem <mabdalmoniem8@gmail.com>
2018-05-23 16:18:10 +0300

    Review Created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,6 +6,8 @@ use App\Model\Review;
 use App\Model\Product;
 use App\Http\Resources\ReviewResource;
 use Illuminate\Http\Request;
+use App\Http\Requests\ReviewRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReviewController extends Controller
 {
@@ -35,9 +37,13 @@ class ReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ReviewRequest $request, Product $product)
     {
-        //
+        $review = new Review($request->all());
+        $product->reviews()->save($review);
+        return response([
+            'data' => new ReviewResource($review)
+        ], Response::HTTP_CREATED);
     }
 
     /**
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Model\Product;
 class Review extends Model
 {
+    protected $fillable = ['customer', 'star', 'review'];
     public function product() {
         return $this->belongsTo(Product::class);
     }
